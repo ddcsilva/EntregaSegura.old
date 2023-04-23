57ee88627e0f2724ba42f01a4ea2708506e4295a
Danilo <danilo.silva@msn.com>
2023-04-23 15:07:07 -0300

    Adicionada a classe BaseEntity como a entidade base para os modelos de negócios do sistema.

---
+++ api/EntregaSegura/src/EntregaSegura.Business/Models/BaseEntity.cs
@@ -0,0 +1,22 @@
+namespace EntregaSegura.Business.Models;
+
+/// <summary>
+/// Classe base para as entidades do sistema.
+/// </summary>
+public abstract class BaseEntity
+{
+    /// <summary>
+    /// Construtor padrão.
+    /// </summary>
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+        DataCriacao = DateTime.Now;
+        Active = true;
+    }
+
+    public Guid Id { get; set; }
+    public DateTime DataCriacao { get; set; }
+    public DateTime? DataUltimaModificacao { get; set; }
+    public bool Active { get; set; }
+}
