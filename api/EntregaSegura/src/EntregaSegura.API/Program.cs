bbb70e845aa40c4e90d770aaede6509d18f9e5b2
Danilo <danilo.silva@msn.com>
2023-04-30 17:33:36 -0300

    Criada a ServiceExtensions e alterada a Program.cs para nova configuração

--- api/EntregaSegura/src/EntregaSegura.API/Program.cs
+++ api/EntregaSegura/src/EntregaSegura.API/Program.cs
@@ -1,29 +1,34 @@
-using EntregaSegura.Data.Contexts;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpOverrides;
+using EntregaSegura.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<EntregaSeguraContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("EntregaSeguraConnection"),
-    b => b.MigrationsAssembly("EntregaSegura.Data"));
-});
-
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigurarCors();
+builder.Services.ConfigurarIntegraçãoComIIS();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
