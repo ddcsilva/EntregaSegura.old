72226fecc4e2766960f9ecdc1ac8b7aa6631644d
Danilo <danilo.silva@msn.com>
2023-04-30 19:45:55 -0300

    Classe de contexto e a conexão com o banco de dados

---
+++ api/EntregaSegura/src/EntregaSegura.API/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,21 @@
+using EntregaSegura.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EntregaSegura.API.ContextFactory;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    public RepositoryContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlServer(configuration.GetConnectionString("SqlServerConnection"));
+
+        return new RepositoryContext(builder.Options);
+    }
+}
